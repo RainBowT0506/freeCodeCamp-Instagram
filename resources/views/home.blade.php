3655c407a8e315adf2ab150e9cee25ae712f423c
RainBowT <rainbowt0506@gmail.com>
2024-01-08 02:09:50 +0800

    feat: Designing the UI from Instagram
    - Modified the layout of the user profile section.
      - Adjusted the structure for better responsiveness.
      - Added styling to the profile image and user details.
    - Updated the article section layout.
      - Displayed a row of three images with padding and dimensions.
    - Organized the content within the container for improved readability.
    - Added links to the user's GitHub and HackMD profiles.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        {{-- header --}}
+        <div class="row flex">
+            <div class="col-3 p-5 flex items-center">
+                <img src="/image/avatar.jpg" class=" h-40 w-40 rounded-full">
+            </div>
+            <div class="col-9 pl-10 pt-10 items-center">
+                <div>
+                    <h1 class="font-bold text-2xl">rainbowt0506</h1>
                 </div>
+                <div class="flex pt-3">
+                    <div class="pr-3"><strong class="pr-2">9</strong>posts</div>
+                    <div class="pr-3"><strong class="pr-2">99</strong>followers</div>
+                    <div class="pr-3"><strong class="pr-2">999</strong>following</div>
+                </div>
+                <div class="pt-4 font-bold">RainBowT.org</div>
+                <div class="max-w-2xl">我叫林承億，一位擁有豐富經驗的全端工程師，目前在香港商團博手機應用有限公司擔任全端工程師一職。我熱愛將科技應用於實際場景，專攻Android開發、React
+                    Native開發和全端開發。我積極進修，不斷提升技能，以提供最優質的應用程式解決方案。
+
+                    我目前正在攻讀國立中興大學資料科學與資訊計算研究所碩士學位，展現了我深入理解並應用資訊科學的承諾。擁有5~6年的工作經驗，期望能擔任Android Developer、React Native
+                    Developer或Full Stack Developer等職稱。
+
+                    以我的卓越專業背景和豐富的工作經歷，我是一位值得信賴的技術專家，致力於打造高品質、創新且競爭力十足的軟體解決方案。
+                </div>
+                <div><a href="#">https://github.com/RainBowT0506</a></div>
+                <div><a href="#">https://hackmd.io/@6USSWVbIT3KHUUNCPK8Pqw</a></div>
             </div>
         </div>
+
+        {{-- article --}}
+        <div class="row pt-10 flex">
+            <div class="col-4"><img src="/image/painting1.jpg" class="p-2 h-40 w-40 "></div>
+            <div class="col-4"><img src="/image/painting2.jpg" class="p-2 h-40 w-40 "></div>
+            <div class="col-4"><img src="/image/painting3.jpg" class="p-2 h-40 w-40 "></div>
+        </div>
     </div>
-</div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,15 +17,18 @@
     <!-- Scripts -->
     @vite('resources/css/app.css')
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <img src="/svg/instagramLogo.svg" style="height: 20px; padding-right: 5px; border-right: 1px solid #333;">
-                    <div style="padding-left: 5px;">Instagram</div>
+                <a class="navbar-brand flex align-items-center" href="{{ url('/') }}">
+                    <img class="h-5 pr-3" src="/svg/instagramLogo.svg">
+                    <div class="pl-3 h-5 border-l border-solid border-gray-800">Instagram</div>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -51,13 +55,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -78,4 +83,5 @@
         </main>
     </div>
 </body>
+
 </html>
