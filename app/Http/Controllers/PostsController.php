e6d2967b9f3d2168ca7fe9dbc162cc14c5206ff1
RainBowT <rainbowt0506@gmail.com>
2024-01-13 02:03:42 +0800

    feat: Creating Through a Relationship
    Apply 'auth' middleware to controller constructor

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
 
+    public function __construct(){
+        $this->middleware("auth");
+    }
+
     public function create()
     {
         return view("posts.create");
