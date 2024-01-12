5e4f893459b93d1024685a48bec75f7a9ffcd696
RainBowT <rainbowt0506@gmail.com>
2024-01-12 22:22:19 +0800

    feat: Adding Eloquent Relationships
    - Define 'belongsTo' relationship in Profile model pointing to User
    - Define 'hasOne' relationship in User model pointing to Profile
    - Use Laravel tinker to create a new Profile with title and description
    - Associate the profile with a user and set a URL
    - Save changes and push the updated user with the associated profile URL

--- app/Models/profile.php
+++ app/Models/profile.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class profile extends Model
+class Profile extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,17 +16,10 @@
                     <div class="pr-3"><strong class="pr-2">99</strong>followers</div>
                     <div class="pr-3"><strong class="pr-2">999</strong>following</div>
                 </div>
-                <div class="pt-4 font-bold">RainBowT.org</div>
-                <div class="max-w-2xl">我叫林承億，一位擁有豐富經驗的全端工程師，目前在香港商團博手機應用有限公司擔任全端工程師一職。我熱愛將科技應用於實際場景，專攻Android開發、React
-                    Native開發和全端開發。我積極進修，不斷提升技能，以提供最優質的應用程式解決方案。
-
-                    我目前正在攻讀國立中興大學資料科學與資訊計算研究所碩士學位，展現了我深入理解並應用資訊科學的承諾。擁有5~6年的工作經驗，期望能擔任Android Developer、React Native
-                    Developer或Full Stack Developer等職稱。
-
-                    以我的卓越專業背景和豐富的工作經歷，我是一位值得信賴的技術專家，致力於打造高品質、創新且競爭力十足的軟體解決方案。
+                <div class="pt-4 font-bold">{{ $user->profile->title }}</div>
+                <div class="max-w-2xl">{{ $user->profile->description }}
                 </div>
-                <div><a href="#">https://github.com/RainBowT0506</a></div>
-                <div><a href="#">https://hackmd.io/@6USSWVbIT3KHUUNCPK8Pqw</a></div>
+                <div><a href="#">{{ $user->profile->url }}</a></div>
             </div>
         </div>
 
