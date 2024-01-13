1d4c602487113d89a2357e44fb087166e09b92e1
RainBowT <rainbowt0506@gmail.com>
2024-01-14 01:11:36 +0800

    feat: Editing the Profile Image
    - Update 'update' method to handle image uploads and resizing
    - Modify migration to include 'image' column in 'profiles' table
    - Refresh migration to apply the changes to the database schema

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class ProfileController extends Controller
 {
@@ -31,7 +33,30 @@ class ProfileController extends Controller
             'image' => '',
         ]);
 
-        auth()->user()->profile->update($data);
+        if (request('image')) {
+            $imgPath['image'] = request('image')->store('profile', 'public');
+
+            // create image manager with desired driver
+            $manager = new ImageManager(new Driver());
+
+            // read image from file system
+            $image = $manager->read('storage/' . $imgPath['image']);
+
+            // resize image proportionally to 300px width
+            $image->scale(width: 1200, height: 1200);
+            $image->save();
+        }
+
+        // dd(array_merge(
+        //     $data,
+        //     ['image' => $imgPath]
+        // ));
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imgPath]
+        ));
+
 
         return redirect("/profile/{$user->id}");
     }
